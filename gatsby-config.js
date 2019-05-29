30b612e0e31a334942edba3cf6f761fb0fd6a8c4
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2019-05-29 23:55:43 +0800

    refactor: iconの修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `contents/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `contents/images/icon/hk-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
